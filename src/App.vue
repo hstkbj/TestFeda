1750ddb528f02742c24e4ab518efb990533e4f5f
hstkbj <hstkbj@asiso.io>
2024-12-18 16:35:17 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Test FedaPay</h1>
+    <button class="btn btn-primary"  id="pay-btn">Payer 1000</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+/* global FedaPay */
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      
+    }
+  },
+  mounted(){
+    if (window.FedaPay) {
+      console.log("FedaPay est chargé :", window.FedaPay);
+    } else {
+      console.error("FedaPay n'est pas chargé !");
+    }
+    this.testfunction()
+  },
+  methods:{
+    testfunction(){
+        FedaPay.init("#pay-btn",{
+          public_key: 'pk_sandbox_L4pS0w5ats9iXVhDv44P3OkY',
+          transaction: {
+            amount: 1000,
+            description: 'Acheter mon produit'
+          },
+          customer: {
+            email: 'johndoe@gmail.com',
+            lastname: 'Doe',
+            firstname: 'John',
+          },
+          onComplete:({reason,transaction})=>{
+            console.log(reason, transaction)
+
+            if (transaction.status === "approved") {
+              console.log(transaction.status)
+            }
+          }
+        })
+    }
   }
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
